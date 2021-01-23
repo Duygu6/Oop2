9648cfae20c88980b081a8fcfd0691ce7f8256cc
Duygu6 <pahliduygu@gmail.com>
2021-01-24 00:06:56 +0300

    Add project files.

---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();  
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaselLoggerService();
+            ILoggerService fileLoogerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            DatabaselLoggerService loggerService = new DatabaselLoggerService();
+            basvuruManager.BasvuruYap(konutKrediManager, loggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager,konutKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        } 
+    }
+}
---
+++ Oop2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 5;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Duygu";
+            musteri1.Soyadi = "Pahli";
+            musteri1.TcNo = "1234548";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "154851";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "15451";
+
+            Musteri musteri3 = new GercekMusteri(); //bellekteki referans no
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+           
+        }
+    }
+}
